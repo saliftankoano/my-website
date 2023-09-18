0fe1532bdc9962b80c2eb60e1522695aa9158e62
Salif Tankoano <tanksalif@gmail.com>
2023-09-17 21:14:42 -0400

    update skills section responsivness

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import Intro from './components/Intro/Intro';
 import Navbar from './components/Navbar/Navbar';
-
+import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Intro/>
     <Navbar/>
+    <Intro/>
+    <Skills/>
+    <Projects/>
+    
   </React.StrictMode>
 );
 
